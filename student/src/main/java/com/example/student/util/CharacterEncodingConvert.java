db9938c73b49fac4a06875b8806b890576b841b2
zhaochaoyue <SVn12sjwl>
2020-09-23 20:31:53 +0800

    天气视频源修改

---
+++ student/src/main/java/com/example/student/util/CharacterEncodingConvert.java
@@ -0,0 +1,86 @@
+package com.example.student.util;
+
+/**
+ * @description: CharacterEncodingConvert
+ * @date: 2020/9/22 下午5:12
+ * @author: zcy
+ * @version: 1.0
+ */
+public class CharacterEncodingConvert {
+    /**
+     * 【中文转Unicode】
+     * @param str
+     * @return 返回转码后的字符串 - Unicode格式
+     */
+    public static String chineseToUnicode(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            int chr1 = (char) str.charAt(i);
+            // 汉字范围 \u4e00 - \u9fa5 (中文)
+            if (chr1 >= 19968 && chr1 <= 171941) {
+                result += "\\u" + Integer.toHexString(chr1);
+            } else {
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 【判断是否为中文字符】
+     * @param c
+     * @return 返回判断结果 - boolean类型
+     */
+    public static boolean isChinese(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 【Unicode转中文】
+     * @param unicode
+     * @return 返回转码后的字符串 - 中文格式
+     */
+    public static String unicodeToChinese(final String unicode) {
+        StringBuffer string = new StringBuffer();
+        String[] hex = unicode.split("\\\\u");
+        for (int i = 0; i < hex.length; i++) {
+            try {
+                // 汉字范围 \u4e00-\u9fa5 (中文)
+                if(hex[i].length()>=4){//取前四个，判断是否是汉字
+                    String chinese = hex[i].substring(0, 4);
+                    try {
+                        int chr = Integer.parseInt(chinese, 16);
+                        boolean isChinese = isChinese((char) chr);
+                        //转化成功，判断是否在  汉字范围内
+                        if (isChinese){//在汉字范围内
+                            // 追加成string
+                            string.append((char) chr);
+                            //并且追加  后面的字符
+                            String behindString = hex[i].substring(4);
+                            string.append(behindString);
+                        }else {
+                            string.append(hex[i]);
+                        }
+                    } catch (NumberFormatException e1) {
+                        string.append(hex[i]);
+                    }
+                }else{
+                    string.append(hex[i]);
+                }
+            } catch (NumberFormatException e) {
+                string.append(hex[i]);
+            }
+        }
+        return string.toString();
+    }
+
+}
--- student/src/test/java/com/example/http/WeatherHttp.java
+++ student/src/test/java/com/example/http/WeatherHttp.java
@@ -1,6 +1,7 @@
 package com.example.http;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.student.util.CharacterEncodingConvert;
 import com.example.student.util.DateTimeUtils;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,11 +19,11 @@ import org.jsoup.select.Elements;
  * @version: 1.0
  */
 public class WeatherHttp {
-    private   static final OkHttpClient okHttpClient = new OkHttpClient();
-    private static final String HTTPS_UR = "http://tv.cntv.cn/videoset/VSET100374596541";
+    private static final OkHttpClient okHttpClient = new OkHttpClient();
+    private static final String HTTPS_UR = "https://search.cctv.com/ifsearch.php?page=1&qtext=%E5%A4%A9%E6%B0%94%E9%A2%84%E6%8A%A5&sort=date&pageSize=20&type=video&vtime=-1&datepid=3&channel=CCTV%E5%85%B6%E4%BB%96&pageflag=0&qtext_str=%E5%A4%A9%E6%B0%94%E9%A2%84%E6%8A%A5";
     private static final String VIDEO_URL = "https://vod.cntv.lxdns.com/flash/mp4video63/TMS/%s/%s/%s/%s_h264418000nero_aac32.mp4";
 
-    public static void main(String[] args)throws Exception {
+    /*public static void main(String[] args)throws Exception {
         Request request = new Request.Builder().url(HTTPS_UR).build();
         String string = okHttpClient.newCall(request).execute().body().string();
         Document document = Jsoup.parseBodyFragment(string);
@@ -41,5 +42,33 @@ public class WeatherHttp {
         String month = date.substring(4, 6);
         String day = date.substring(6, 8);
         String videoUrl = String.format(VIDEO_URL, year, month, day, md5);
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        Request request = new Request.Builder().url(HTTPS_UR).build();
+        String string = okHttpClient.newCall(request).execute().body().string();
+        System.out.println(string);
+        String unicodeStr = CharacterEncodingConvert.unicodeToChinese(string);
+        System.out.println(unicodeStr);
+        JSONObject object = JSONObject.parseObject(unicodeStr);
+        JSONObject json = object.getJSONArray("list").getJSONObject(0);
+        String title = json.getString("all_title");
+        String[] s = title.split(" ");
+        String name = s[0];
+        String date = s[1];
+        String year = date.substring(0, 4);
+        String month = date.substring(4, 6);
+        String day = date.substring(6, 8);
+        String src = json.getString("imglink");
+        String[] imglinks = src.split("/");
+        String[] split = imglinks[imglinks.length - 1].split("-");
+        String md5 = split[0];
+        String videoUrl = String.format(VIDEO_URL, year, month, day, md5);
+        jsonObject.put("name", name);
+        jsonObject.put("src", src);
+        jsonObject.put("videoUrl", videoUrl);
+        jsonObject.put("date", date);
+        System.out.println(JSONObject.toJSONString(jsonObject));
     }
 }
